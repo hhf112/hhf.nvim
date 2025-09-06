3199fd80314d5cd9382368bb683b640bb8cdbaed
harsh <hanzo4679@gmail.com>
2025-09-06 05:53:20 +0000

    refactor

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,53 +1,80 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+  -- {
+  --   "stevearc/conform.nvim",
+  --   -- event = 'BufWritePre', -- uncomment for format on save
+  --   opts = require "configs.conform",
+  -- },
 
-  -- These are some examples, uncomment them if you want to see them work!
+  --dev {
+  {
+    "themaxmarchuk/tailwindcss-colors.nvim",
+    lazy = false,
+  },
+  --}
+
+  --AI {
+  {
+    "supermaven-inc/supermaven-nvim",
+    lazy = true,
+    config = function()
+      require("supermaven-nvim").setup {
+        keymaps = {
+          accept_suggestion = "<C-]>",
+          clear_suggestion = "<C-[>",
+          accept_word = "<C-l>",
+        },
+        colors = {
+          suggestion_color = "#4c4c55",
+          cterm = 244,
+        },
+        ignore_filetypes = { cpp = true },
+        log_level = "info", -- set to "off" to disable logging completely
+        disable_inline_completion = false, -- disables inline completion for use with cmp
+        disable_keymaps = false, -- disables built in keymaps for more manual control
+        condition = function()
+          return true
+        end, -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
+      }
+    end,
+  },
+  --}
+
+  -- files {
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+      },
+    },
+    config = function()
+      return require "configs.telescope"
+    end,
+  },
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@jype oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
+  -- }
+
+  --lsp
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
-  ---
-  --------------------------------------~!DEV---
-  --UTILITY
-  {
-    "themaxmarchuk/tailwindcss-colors.nvim",
-    lazy = false,
-  },
 
-  --AI
-  {
-    "supermaven-inc/supermaven-nvim",
-    lazy = true,
-    config = function()
-      require("supermaven-nvim").setup(
-        {
-          keymaps = {
-            accept_suggestion = "<C-]>",
-            clear_suggestion = "<C-[>",
-            accept_word = "<C-l>",
-          },
-          colors = {
-            suggestion_color = "#4c4c55",
-            cterm = 244,
-          },
-          ignore_filetypes = { cpp = true },
-          log_level = "info",                -- set to "off" to disable logging completely
-          disable_inline_completion = false, -- disables inline completion for use with cmp
-          disable_keymaps = false,           -- disables built in keymaps for more manual control
-          condition = function()
-            return true
-          end -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
-        })
-    end,
-  },
-
-  --LSP
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -67,11 +94,11 @@ return {
         "pyright",
         "gopls",
         "tailwindcss",
-      }
-    }
+      },
+    },
   },
 
-  --DEBUGGER
+  --debuggger
   {
     "jay-babu/mason-nvim-dap.nvim",
     event = "VeryLazy",
@@ -88,8 +115,8 @@ return {
     event = "VeryLazy",
     dependencies = "mfussenegger/nvim-dap",
     config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
+      local dap = require "dap"
+      local dapui = require "dapui"
       dapui.setup()
       dap.listeners.after.event_initialized["dapui_config"] = function()
         dapui.open()
@@ -100,63 +127,10 @@ return {
       dap.listeners.before.event_exited["dapui_config"] = function()
         dapui.close()
       end
-    end
+    end,
   },
   {
     "mfussenegger/nvim-dap",
   },
   { "nvim-neotest/nvim-nio" },
-
-  -----------------------------------------~!EDITOR-----------
-
-
-
-  ---ADDITIONAL UI-------------------
-  {
-    "xiyaowong/transparent.nvim",
-    lazy = true,
-  },
-
-  --FILE MANAGEMENT
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' },
-
-    },
-    config = function()
-      return require "configs.telescope"
-    end,
-  },
-  {
-    'stevearc/oil.nvim',
-    ---@module 'oil'
-    ---@jype oil.SetupOpts
-    opts = {},
-    -- Optional dependencies
-    dependencies = { { "echasnovski/mini.icons", opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-    lazy = false,
-
-    -- config = function()
-    --   require "custom.configs.oil"
-    -- end
-  },
-  --UTILITY
-
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
 }
